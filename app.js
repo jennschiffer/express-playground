3083ce8dff76fac62667932f988893c5223709e1
Jenn Schiffer <jenn@pancaketheorem.com>
2014-04-16 17:10:10 -0400

    tabs to spaces, thanks for nothing sublime

--- app.js
+++ app.js
@@ -5,25 +5,25 @@ var nib = require('nib');
 var app = express();
 
 function compile(str, path) {
-	return stylus(str)
-		.set('filename', path)
-		.use(nib());
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
 }
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(stylus.middleware({ 
-		src: __dirname + '/public',
-		compile: compile
-	})
+    src: __dirname + '/public',
+    compile: compile
+  })
 );
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-	res.render('index', 
-		{
-			title: 'Home'
-		}
-	);
+  res.render('index', 
+    {
+      title: 'home'
+    }
+  );
 });
 app.listen('5678');
